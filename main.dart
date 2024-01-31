01ff38e80e9d393dae7bd5caeb1abee2f0a6afc9
friendlyphoenix <yousuf.friendlyphoenix@gmail.com>
2024-01-31 16:39:07 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,8 +1,7 @@
 import 'bank_account.dart';
+import 'student.dart';
 
 void main() {
-
-
   BankAccount account1 = BankAccount(1001, 5000.0, "Savings Plus", 7.5);
   BankAccount account2 = BankAccount(1002, 10000.0, "Savings", 6.0);
 
@@ -17,4 +16,25 @@ void main() {
   account2.withdraw(1500);
   account2.addInterest();
   account2.display();
+
+  print("\n");
+
+  Student student1 = Student("Yousuf", "1111", []);
+  Student student2 = Student("Ali", "1112", []);
+
+  student1.addCourse("Math");
+  student1.addCourse("Science");
+  student2.addCourse("History");
+  student2.addCourse("English");
+
+  print("Initial Courses:");
+  student1.displayCourses();
+  student2.displayCourses();
+
+  student1.dropCourse("Science");
+  student2.dropCourse("English");
+
+  print("\nUpdated Courses:");
+  student1.displayCourses();
+  student2.displayCourses();
 }
